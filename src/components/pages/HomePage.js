f7ad492e7041c4d08d95485e8d9c24483ed93f4a
Tomas <tomasg88@gmail.com>
2020-06-20 20:16:37 -0300

    random shape

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Page from "../Page/Page";
+import Shape from "../shapes/shape";
 
 const HomePage = () => {
 	return (
 		<Page background="purple">
 			<h4 className="animate__animated animate__swing">Home</h4>
+			<Shape />
 		</Page>
 	);
 };
