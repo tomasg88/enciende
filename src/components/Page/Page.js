183dd112eff1f033df88d904d78bb6bfdbac6095
Tomas <tomasg88@gmail.com>
2020-06-20 21:10:23 -0300

    main container is now material grid

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Grid, makeStyles } from "@material-ui/core";
+import clsx from "clsx";
 
-const Page = ({ children, background }) => (
-	<div className={`back-main-${background} pt-10 h-screen`}>{children}</div>
-);
+const useStyles = makeStyles((theme) => ({
+	mainGrid: {
+		width: "100vw",
+		height: "100vh",
+	},
+}));
+
+const Page = ({ children, background }) => {
+	const classes = useStyles();
+	return (
+		<Grid
+			container
+			alignItems="center"
+			justify="center"
+			direction="column"
+			className={clsx(classes.mainGrid, `back-main-${background}`)}
+		>
+			{children}
+		</Grid>
+	);
+};
 
 export default Page;
