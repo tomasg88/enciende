bb4f8bc8b163a1cb0ffc536958e4d621a767e015
Tomas <tomasg88@gmail.com>
2020-06-20 21:09:59 -0300

    styling to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<div className="absolute flex justify-center w-screen pl-10 pr-10 text-xl">
+		<div className="absolute flex justify-center w-screen pl-10 pr-10 text-xl items-center h-10">
 			<Link className="pl-5 pr-5" to="/">
 				Home
 			</Link>
