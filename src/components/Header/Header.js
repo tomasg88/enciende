e995896e4ec58080da3b6b83b8957092855fedda
Tomas <tomasg88@gmail.com>
2020-06-22 11:11:22 -0300

    Add nav bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 const Header = () => {
+	const [elevation, setElevation] = useState(1);
 	return (
-		<div className="absolute flex justify-center w-screen pl-10 pr-10 text-xl items-center h-10">
-			<Link className="pl-5 pr-5" to="/">
-				Home
-			</Link>
-			<Link className="pl-5 pr-5" to="/about">
-				Sobre mi
-			</Link>
-			<Link className="pl-5 pr-5" to="/services">
-				Servicios
-			</Link>
-		</div>
+		<AppBar
+			position="absolute"
+			color="transparent"
+			elevation={elevation}
+			onMouseEnter={() => setElevation(3)}
+			onMouseLeave={() => setElevation(1)}
+		>
+			<Toolbar className="flex justify-center w-screen pl-10 pr-10 text-xl items-center h-10 z-10 border-red-300">
+				<Link className="pl-5 pr-5" to="/">
+					<Typography variant="subtitle1" color="">
+						Home
+					</Typography>
+				</Link>
+				<Link className="pl-5 pr-5" to="/about">
+					<Typography variant="subtitle1" color="">
+						Sobre mi
+					</Typography>
+				</Link>
+				<Link className="pl-5 pr-5" to="/services">
+					<Typography variant="subtitle1" color="">
+						Servicios
+					</Typography>
+				</Link>
+			</Toolbar>
+		</AppBar>
 	);
 };
 
